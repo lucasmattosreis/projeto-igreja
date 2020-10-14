d31da38c1a9ced1adec6c0da4abe9c7044f16b6a
Lucas Reis <luck_mattos.reis@hotmail.com>
2020-10-14 11:49:35 -0300

    dashboard completa

--- resources/views/layouts/sistema/cabecalho.blade.php
+++ resources/views/layouts/sistema/cabecalho.blade.php
@@ -17,8 +17,20 @@
 
     <!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM STYLES -->
     <link href="{{asset('sistema/plugins/apex/apexcharts.css')}}" rel="stylesheet" type="text/css">
-    <link href="{{asset('sistema/assets/css/dashboard/dash_2.css')}}" rel="stylesheet" type="text/css" />
     <!-- END PAGE LEVEL PLUGINS/CUSTOM STYLES -->
+
+    <style>
+        /*
+            The below code is for DEMO purpose --- Use it if you are using this demo otherwise Remove it
+        */
+        /*.navbar .navbar-item.navbar-dropdown {
+            margin-left: auto;
+        }*/
+        .layout-px-spacing {
+            min-height: calc(100vh - 184px)!important;
+        }
+
+    </style>
 
 </head>
 <body class="alt-menu sidebar-noneoverflow">
@@ -199,50 +211,101 @@
     </div>
     <!--  END NAVBAR  -->
 
-    <!--  BEGIN MAIN CONTAINER  -->
-    <div class="main-container" id="container">
+        <!--  BEGIN MAIN CONTAINER  -->
+        <div class="main-container" id="container">
 
-        <div class="overlay"></div>
-        <div class="search-overlay"></div>
-
-        <!--  BEGIN TOPBAR  -->
-        <div class="topbar-nav header navbar" role="banner">
-            <nav id="topbar">
-                <ul class="navbar-nav theme-brand flex-row  text-center">
-                    <li class="nav-item theme-logo">
-                        <a href="index.html">
-                            <img src="{{asset('sistema/assets/img/90x90.jpg')}}" class="navbar-logo" alt="logo">
-                        </a>
-                    </li>
-                    <li class="nav-item theme-text">
-                        <a href="index.html" class="nav-link"> CORK </a>
-                    </li>
-                </ul>
-
-                <ul class="list-unstyled menu-categories" id="topAccordion">
-
-                    <li class="menu single-menu active">
-                        <a href="#dashboard" data-toggle="collapse" aria-expanded="true" class="dropdown-toggle autodroprown">
-                            <div class="">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
-                                <span>Dashboard</span>
-                            </div>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
-                        </a>
-                        <ul class="collapse submenu list-unstyled" id="dashboard" data-parent="#topAccordion">
-                            <li class="active">
-                                <a href="index.html"> Analytics </a>
-                            </li>
-                            <li>
-                                <a href="index2.html"> Sales </a>
-                            </li>
-                        </ul>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-        <!--  END TOPBAR  -->
-
-        <!--  BEGIN CONTENT PART  -->
-        <div id="content" class="main-content">
-            <div class="layout-px-spacing">
+            <div class="overlay"></div>
+            <div class="search-overlay"></div>
+    
+            <!--  BEGIN TOPBAR  -->
+            <div class="topbar-nav header navbar" role="banner">
+                <nav id="topbar">
+                    <ul class="navbar-nav theme-brand flex-row  text-center">
+                        <li class="nav-item theme-logo">
+                            <a href="index.html">
+                                <img src="assets/img/90x90.jpg" class="navbar-logo" alt="logo">
+                            </a>
+                        </li>
+                        <li class="nav-item theme-text">
+                            <a href="index.html" class="nav-link"> CORK </a>
+                        </li>
+                    </ul>
+    
+                    <ul class="list-unstyled menu-categories" id="topAccordion">
+    
+                        <li class="menu single-menu">
+                            <a href="#menu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle autodroprown">
+                                <div class="">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>
+                                    <span>Menu 1</span>
+                                </div>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
+                            </a>
+                            <ul class="collapse submenu list-unstyled" id="menu1" data-parent="#topAccordion">
+                                <li>
+                                    <a href="javascript:void(0);"> Submenu 1 </a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0);"> Submenu 2 </a>
+                                </li>
+                            </ul>
+                        </li>
+    
+    
+                        <li class="menu single-menu">
+                            <a href="#menu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle autodroprown">
+                                <div class="">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-box"><path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"></path><polyline points="3.27 6.96 12 12.01 20.73 6.96"></polyline><line x1="12" y1="22.08" x2="12" y2="12"></line></svg>
+                                    <span>Menu 2</span>
+                                </div>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
+                            </a>
+                            <ul class="collapse submenu list-unstyled" id="menu2" data-parent="#topAccordion">
+                                <li>
+                                    <a href="javascript:void(0);"> Submenu 1 </a>
+                                </li>
+                                <li>
+                                    <a href="javascript:void(0);"> Submenu 2 </a>
+                                </li>
+                                <li class="sub-sub-submenu-list">
+                                    <a href="#sub-sub-category" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle"> Submenu 3 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right"><polyline points="9 18 15 12 9 6"></polyline></svg> </a>
+                                    <ul class="collapse list-unstyled sub-submenu" id="sub-sub-category" data-parent="#menu"> 
+                                        <li>
+                                            <a href="javascript:void(0);"> Sub-Submenu 1 </a>
+                                        </li>
+                                        <li>
+                                            <a href="javascript:void(0);"> Sub-Submenu 2 </a>
+                                        </li>
+                                        <li>
+                                            <a href="javascript:void(0);"> Sub-Submenu 3 </a>
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </li>
+    
+                        <li class="menu single-menu active">
+                            <a href="#starter-kit" data-toggle="collapse" aria-expanded="true" class="dropdown-toggle autodroprown">
+                                <div class="">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus-circle"><circle cx="12" cy="12" r="10"></circle><line x1="12" y1="8" x2="12" y2="16"></line><line x1="8" y1="12" x2="16" y2="12"></line></svg>
+                                    <span>Starter Kit</span>
+                                </div>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
+                            </a>
+                            <ul class="collapse submenu list-unstyled" id="starter-kit" data-parent="#topAccordion">
+                                <li class="active">
+                                    <a href="starter_kit_blank_page.html"> Blank Page </a>
+                                </li>
+                                <li>
+                                    <a href="starter_kit_breadcrumb.html"> Breadcrumb </a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <!--  END TOPBAR  -->
+            
+            <!--  BEGIN CONTENT AREA  -->
+            <div id="content" class="main-content">
+                <div class="layout-px-spacing">
--- resources/views/sistema/index.blade.php
+++ resources/views/sistema/index.blade.php
@@ -1,5 +1,18 @@
 @extends('layouts.sistema.layoutsistema')
 
 @section('conteudo')
-  <h1>ola mundo</h1>
+<div class="row layout-top-spacing">
+                    
+  <div class="col-xl-12 col-lg-12 col-md-12 col-12 layout-spacing">
+      <div class="widget-content-area br-4">
+          <div class="widget-one">
+
+              <h6>Igreja Assembleia de Deus - Retiro - Volta Redonda - RJ</h6>
+
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/xzwHgqlNx6I" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          </div>
+      </div>
+  </div>
+
+</div>
 @endsection
